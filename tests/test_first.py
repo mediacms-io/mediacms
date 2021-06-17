83e6e115a83549f65f0745c17b214dfae663edb1
Markos Gogoulos <markos@orfium.com>
2021-06-17 22:22:43 +0300

    tests

--- tests/test_first.py
+++ tests/test_first.py
@@ -5,9 +5,9 @@ from users.models import User
 
 class UserTests(TestCase):
     def setUp(self):
-        user = User.objects.create(username='markos', email='sap@ios.gr')
-        user = User.objects.create(username='nikos', email='sap@ios.agr')
-    
+        User.objects.create(username='markos', email='sap@ios.gr')
+        User.objects.create(username='nikos', email='sap@ios.agr')
+
     def test_users(self):
-        self.assertEqual(User.objects.filter().count(), 2, 'prb')   
-        self.assertEqual(User.objects.filter().count(), 22, 'problem') 
+        self.assertEqual(User.objects.filter().count(), 2, 'prb')
+        self.assertEqual(User.objects.filter().count(), 22, 'problem')
