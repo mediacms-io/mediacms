8f2aa57e0306adc9ff030c2d6d6acb6d7fc938f0
Ubuntu <shubhank@my-hostings.nxfutj5b2tlubjykddwgszqteb.bx.internal.cloudapp.net>
2021-06-18 19:23:34 +0000

    Fix lint

--- tests/accounts/test_forms.py
+++ tests/accounts/test_forms.py
@@ -1,10 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
+
 class TestBrowser1(LiveServerTestCase):
     def test_example(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
-        driver = webdriver.Chrome( executable_path= r"./chromedriver", options=options)
+        driver = webdriver.Chrome(executable_path=r"./chromedriver", options=options)
         driver.get(("%s%s" % (self.live_server_url, "/admin/")))
         assert "Log in | Django site admin" in driver.title
