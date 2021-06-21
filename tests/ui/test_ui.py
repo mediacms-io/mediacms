b9fd25adc20994adc1e1500973680be6b5829ca8
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-06-21 15:41:21 +0530

    Testing featured button

---
+++ tests/ui/test_ui.py
@@ -0,0 +1,15 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class TestUI(LiveServerTestCase):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path=r"./chromedriver", options=options)
+
+    def test_featured_url(self, driver):
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='app-sidebar']/div/div/div[1]/nav/ul/li[2]/a"))).click()
+        assert driver.getCurrentUrl() == 'localhost/featured'
