9508a5f6ea8a423e2b75e126664bb6aafa565d48
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-06-29 15:09:51 +0530

    Remove old tests

--- tests/test_selenium_smoke.py
+++ tests/test_selenium_smoke.py
@@ -4,20 +4,11 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class SeleniumTest(TestCase):
-    def test_example(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(executable_path=r"./chromedriver", options=options)
-        driver.get(("%s%s" % (self.live_server_url, "/admin/")))
-        assert "Log in | Django site admin" in driver.title
-
     def setUp(self):
         self.chrome = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
-
         self.chrome.implicitly_wait(10)
 
         self.firefox = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
-
         self.firefox.implicitly_wait(10)
 
     def test_visit_site_with_chrome(self):
