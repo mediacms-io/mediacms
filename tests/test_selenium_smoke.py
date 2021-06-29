093448c14518ecfe23d35c92bf8269b2e05403ae
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-06-29 16:01:04 +0530

    Remove firefox from selenium tests

--- tests/test_selenium_smoke.py
+++ tests/test_selenium_smoke.py
@@ -8,13 +8,6 @@ class SeleniumTest(TestCase):
         self.chrome = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
         self.chrome.implicitly_wait(10)
 
-        self.firefox = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
-        self.firefox.implicitly_wait(10)
-
     def test_visit_site_with_chrome(self):
         self.chrome.get('http://web:80/admin')
         self.assertIn(self.chrome.title, "Log in | Django site admin")
-
-    def test_visit_site_with_firefox(self):
-        self.firefox.get('http://web:80/admin')
-        self.assertIn(self.firefox.title, "Log in | Django site admin")
