337ca3ffa87e345ecb69cb060ad50afacb5640c6
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-07-03 12:50:19 +0530

    Add click and assert current url

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -7,9 +7,11 @@ class SeleniumTest(TestCase):
     def setUp(self):
         self.chrome = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
         self.chrome.implicitly_wait(10)
+        self.chrome.get('http://web/')
 
     def test_visit_site_with_chrome(self):
-        self.chrome.get('http://web')
         home_nav = self.chrome.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/nav/ul/li[1]/a")
         self.assertIn(home_nav.text, "Home")
         self.assertIn(home_nav.get_attribute("href"), 'http://web/')
+        home_nav.click(home_nav)
+        self.assertIn(self.chrome.current_url, "http://web/")
