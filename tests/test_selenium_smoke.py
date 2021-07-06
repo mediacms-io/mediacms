48c5bc8de66799a726cdae492a3ec8ee0f90ee71
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-07-06 16:17:58 +0530

    Remove smoketest

--- tests/test_selenium_smoke.py
+++ tests/test_selenium_smoke.py
@@ -1,14 +1,14 @@
-from django.test import TestCase
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from django.test import TestCase
+# from selenium import webdriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class SeleniumTest(TestCase):
-    def setUp(self):
-        self.chrome = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
-        self.chrome.implicitly_wait(10)
+# class SeleniumTest(TestCase):
+#     def setUp(self):
+#         self.chrome = webdriver.Remote(command_executor='http://selenium_hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
+#         self.chrome.implicitly_wait(10)
 
-    def test_visit_site_with_chrome(self):
-        self.chrome.get('http://web/admin')
-        self.assertIn(self.chrome.title, "Log in | Django site admin")
-        self.chrome.close()
+#     def test_visit_site_with_chrome(self):
+#         self.chrome.get('http://web/admin')
+#         self.assertIn(self.chrome.title, "Log in | Django site admin")
+#         self.chrome.close()
