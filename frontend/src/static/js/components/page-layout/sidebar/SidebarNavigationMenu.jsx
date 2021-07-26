05ef781da711e39ff1148b00197b136f18c78342
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-07-26 16:48:59 +0530

    Add navigation translations for hindi

--- frontend/src/static/js/components/page-layout/sidebar/SidebarNavigationMenu.jsx
+++ frontend/src/static/js/components/page-layout/sidebar/SidebarNavigationMenu.jsx
@@ -52,7 +52,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.featured,
         icon: 'star',
-        text: PageStore.get('config-enabled').pages.featured.title,
+        text: t('Featured'),
         className: 'nav-item-featured',
       });
     }
@@ -64,7 +64,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.recommended,
         icon: 'done_outline',
-        text: PageStore.get('config-enabled').pages.recommended.title,
+        text: t('Recommended'),
         className: 'nav-item-recommended',
       });
     }
@@ -73,7 +73,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.latest,
         icon: 'new_releases',
-        text: PageStore.get('config-enabled').pages.latest.title,
+        text: t('Latest'),
         className: 'nav-item-latest',
       });
     }
@@ -86,7 +86,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.archive.tags,
         icon: 'local_offer',
-        text: PageStore.get('config-enabled').taxonomies.tags.title,
+        text: t('Tags'),
         className: 'nav-item-tags',
       });
     }
@@ -99,7 +99,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.archive.categories,
         icon: 'list_alt',
-        text: PageStore.get('config-enabled').taxonomies.categories.title,
+        text: t('Categories'),
         className: 'nav-item-categories',
       });
     }
@@ -108,7 +108,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.members,
         icon: 'people',
-        text: PageStore.get('config-enabled').pages.members.title,
+        text: t('Members'),
         className: 'nav-item-members',
       });
     }
@@ -128,6 +128,7 @@ export function SidebarNavigationMenu() {
   }
 
   function MainMenuSecondSection() {
+    const { t } = useTranslation();
     const items = [];
 
     if (!isAnonymous) {
@@ -135,7 +136,7 @@ export function SidebarNavigationMenu() {
         items.push({
           link: links.user.addMedia,
           icon: 'video_call',
-          text: 'Upload media',
+          text: t('Upload media'),
           className: 'nav-item-upload-media',
         });
 
@@ -143,7 +144,7 @@ export function SidebarNavigationMenu() {
           items.push({
             link: userPages.media,
             icon: 'video_library',
-            text: 'My media',
+            text: t('My media'),
             className: 'nav-item-my-media',
           });
         }
@@ -153,7 +154,7 @@ export function SidebarNavigationMenu() {
         items.push({
           link: userPages.playlists,
           icon: 'playlist_play',
-          text: 'My playlists',
+          text: t('My playlists'),
           className: 'nav-item-my-playlists',
         });
       }
@@ -163,13 +164,14 @@ export function SidebarNavigationMenu() {
   }
 
   function UserMenuSection() {
+    const { t } = useTranslation();
     const items = [];
 
     if (PageStore.get('config-enabled').pages.history && PageStore.get('config-enabled').pages.history.enabled) {
       items.push({
         link: links.user.history,
         icon: 'history',
-        text: PageStore.get('config-enabled').pages.history.title,
+        text: t('History'),
         className: 'nav-item-history',
       });
     }
@@ -182,7 +184,7 @@ export function SidebarNavigationMenu() {
       items.push({
         link: links.user.liked,
         icon: 'thumb_up',
-        text: PageStore.get('config-enabled').pages.liked.title,
+        text: t('Liked Media'),
         className: 'nav-item-liked',
       });
     }
