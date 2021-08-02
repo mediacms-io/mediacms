833ba862c3b6fc54341c44800b5d8d2f609daf1d
swiftugandan <swiftugandan@gmail.com>
2021-08-02 15:52:11 +0100

    adding pdf viewer

--- frontend/src/static/js/components/media-viewer/PdfViewer.js
+++ frontend/src/static/js/components/media-viewer/PdfViewer.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import PDFViewer from 'pdf-viewer-reactjs'
+import { SiteContext } from '../../utils/contexts/';
+import { formatInnerLink } from '../../utils/helpers/';
+import { MediaPageStore} from '../../utils/stores/';
 
 export default function PdfViewer() {
+  const mediaData = MediaPageStore.get('media-data');
+  const pdfUrl = formatInnerLink(mediaData.original_media_url, SiteContext._currentValue.url);
+  console.log('URL: ', mediaData);
   return (
-    <div className="player-container viewer-pdf-container">
-      <div className="player-container-inner">
-        <span>
-          <span>
-            <i className="material-icons">insert_drive_file</i>
-          </span>
-        </span>
-      </div>
-    </div>
+    <div className='column has-text-centered' id='pdf_viewer'>
+    <PDFViewer
+      document={{
+        url: pdfUrl,
+      }}
+      scale={1.5}
+      scaleStep={0.5}
+      maxScale={5}
+      minScale={0.5}
+      protectContent
+      navbarOnTop={true}
+    />
+  </div>
   );
 }
