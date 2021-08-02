5e212aa9a6e93f75a96399487d440bf9fac7c138
Markos Gogoulos <markos@orfium.com>
2021-08-02 21:57:46 +0300

    url tests

--- tests/api/test_user_whoami.py
+++ tests/api/test_user_whoami.py
@@ -1,31 +1,26 @@
 from django.test import Client, TestCase
-from faker import Factory
 
-from users.models import User
+from files.tests import create_account
 
-faker = Factory.create()
+API_V1_LOGIN_URL = '/api/v1/whoami'
 
 
 class TestUserWhoami(TestCase):
     def setUp(self):
-        email = faker.email()
-        self.username = email.split('a')[0]
-        self.password = faker.password()
-        user = User.objects.create(username=self.username, email=email, name=faker.name(), description=faker.text(), is_superuser=False)
-        user.set_password(self.password)
-        user.save()
+        self.user = create_account()
 
     def test_whoami_endpoint(self):
         client = Client()
-        response = client.get('/api/v1/whoami')
+        response = client.get(API_V1_LOGIN_URL)
         self.assertEqual(
             response.status_code,
             403,
             "Expected 403",
         )
-        user = User.objects.get(username=self.username)
-        client.login(username=self.username, password=self.password)
-        response = client.get('/api/v1/whoami')
+
+        user = self.user
+        client.force_login(user=user)
+        response = client.get(API_V1_LOGIN_URL)
         self.assertEqual(
             response.status_code,
             200,
