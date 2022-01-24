cb75931ec60cdfec9799118a9e56a2359e8a91e6
Guys Network <admin@guys.network>
2022-01-24 04:59:42 +0000

    Transfer to compute box

--- cms/storage_backends.py
+++ cms/storage_backends.py
@@ -26,6 +26,14 @@ class MediaStorage(S3Boto3Storage):
             return p[p.index(self.location)+len(self.location):]
         else:
             return p
+    
+    def _open(self, *args, **kwargs):
+        while len(args) >= 1 and str(args[0]).startswith(self.location):
+            args[0] = self.exact_in(args[0])
+
+        while len(args) >= 2 and str(args[1]).startswith(self.location):
+            args[1] = self.exact_in(args[1])
+        return super(MediaStorage, self)._open(*args, **kwargs)
 
 class StaticMediaStorage(S3Boto3Storage):
     location = settings.AWS_LOCATION
