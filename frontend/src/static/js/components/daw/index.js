042c689ea6538944ffb281401d0b228d83a20e36
Megidd <megiddgit@gmail.com>
2022-02-27 08:02:42 +0330

    Using SiteConsumer for DAW: is it necessary?

--- frontend/src/static/js/components/daw/index.js
+++ frontend/src/static/js/components/daw/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import { SiteContext, SiteConsumer } from '../../utils/contexts';
+import { BrowserCache } from '../../utils/classes/';
+
 export default class Daw extends React.PureComponent {
     constructor(props) {
         super(props);
+
+        this.browserCache = new BrowserCache(SiteContext._currentValue.id, 86400); // Keep cache data "fresh" for one day.
     }
 
     render() {
         return (
             <div className="daw-container" key="daw-container">
-                <span>DAW placeholder</span>
+                <SiteConsumer>
+                {(site) => (
+                  <span>DAW placeholder</span>
+                )}
+              </SiteConsumer>
             </div>
         );
     }
