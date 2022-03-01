4772c163e0e76b1e9c879b534ec7bc3cc6222780
Megidd <megiddgit@gmail.com>
2022-03-01 10:50:22 +0330

    Extra buttons

--- frontend/src/static/js/components/daw/index.js
+++ frontend/src/static/js/components/daw/index.js
@@ -6,10 +6,17 @@ import { saveAs } from "file-saver";
 
 import 'waveform-playlist/styles/playlist.scss';
 
+// For extra buttons.
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Daw() {
   const [ee] = useState(new EventEmitter());
   const [toneCtx, setToneCtx] = useState(null);
   const setUpChain = useRef();
+
+  window.MNS = {} // Namespace to hold vars.
+	MNS.time = 0 // Current media time.
+	MNS.paused = true // Is media paused or playing?
 
   const container = useCallback(
     (node) => {
@@ -99,23 +106,80 @@ export default function Daw() {
         onLoad={handleLoad}
       />
       <main>
-        <div>
-          <button
-            onClick={() => {
-              ee.emit("play");
-            }}
-          >
-            Play
-          </button>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              ee.emit("startaudiorendering", "wav");
-            }}
-          >
-            Download
-          </button>
+        <div class="controls-groups">
+          <div class="controls-group">
+            <div class="btn-group">
+              <button type="button" id="btn-play" class="btn btn-outline-success" title="Play/Pause"
+                onClick={() => {
+                  if (MNS.paused) {
+                    ee.emit("play");
+                    // TODO: play video.
+                  } else {
+                    ee.emit("pause");
+                    // TODO: pause video.
+                  }
+
+                  // Toggle play/pause.
+                  MNS.paused = !MNS.paused
+                }}
+              >
+                <i class="fas fa-play"></i>
+                <i class="fas fa-pause"></i>
+              </button>
+              <button type="button" id="btn-record" class="btn btn-outline-primary disabled" title="Record">
+                <i class="fas fa-microphone"></i>
+              </button>
+              <button type="button" id="btn-stop" class="btn btn-outline-danger" title="Stop">
+                <i class="fas fa-stop"></i>
+              </button>
+            </div>
+          </div>
+          <div class="controls-group">
+            <div class="btn-group">
+              <button type="button" title="Zoom in" id="btn-zoom-in" class="btn btn-outline-dark">
+                <i class="fas fa-search-plus"></i>
+              </button>
+              <button type="button" title="Zoom out" id="btn-zoom-out" class="btn btn-outline-dark">
+                <i class="fas fa-search-minus"></i>
+              </button>
+            </div>
+            <div class="btn-group btn-playlist-state-group">
+              <button type="button" id="btn-cursor" class="btn btn-outline-dark active" title="Select cursor">
+                <i class="fas fa-headphones"></i>
+              </button>
+              <button type="button" id="btn-select" class="btn btn-outline-dark" title="Select audio region">
+                <i class="fas fa-italic"></i>
+              </button>
+              <button type="button" id="btn-shift" class="btn btn-outline-dark" title="Shift audio in time">
+                <i class="fas fa-arrows-alt-h"></i>
+              </button>
+            </div>
+            <div class="btn-group btn-select-state-group">
+              <button type="button" id="btn-trim-audio"
+                title="Keep only the selected audio region for a track"
+                class="btn btn-outline-primary disabled">
+                Trim
+              </button>
+            </div>
+          </div>
+          <div class="controls-group">
+            <div class="btn-group">
+              <button type="button" title="Clear the playlist's tracks"
+                class="btn btn-clear btn-outline-danger">
+                Clear
+              </button>
+            </div>
+            <div class="btn-group">
+              <button type="button" title="Download the current work as Wav file"
+                class="btn btn-download btn-outline-primary"
+                onClick={() => {
+                  ee.emit("startaudiorendering", "wav");
+                }}
+              >
+                <i class="fas fa-download"></i>
+              </button>
+            </div>
+          </div>
         </div>
         <div ref={container}></div>
       </main>
