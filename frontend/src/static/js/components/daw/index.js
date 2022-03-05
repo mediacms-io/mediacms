c8ee3a67fd50dd4370dc730d04d5191dca99d803
Megidd <megiddgit@gmail.com>
2022-03-05 07:07:14 +0330

    Add audio files by uploding. Errors are thrown.

--- frontend/src/static/js/components/daw/index.js
+++ frontend/src/static/js/components/daw/index.js
@@ -274,6 +274,49 @@ export default function Daw() {
           </div>
         </div>
         <div ref={container}></div>
+        <div class="track-drop" id="track-drop"
+          style="display: flex; align-items: center; flex-direction: column; justify-content: center;"
+          onDragEnter={(event) => {
+            event.preventDefault();
+            event.target.classList.add("drag-enter");
+          }}
+          onDragOver={(event)=>{
+            event.preventDefault();
+          }}
+          onDragLeave={(event)=>{
+            event.preventDefault();
+            event.target.classList.remove("drag-enter");
+          }}
+          onDrop={(event)=>{
+            event.preventDefault();
+            event.target.classList.remove("drag-enter");
+
+            // Fix:
+            // Uncaught TypeError: Cannot read properties of undefined (reading 'dataTransfer')
+            var dropEvent = event //event.originalEvent;
+
+            for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
+              ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
+            }
+          }}
+        >
+          <button type="button" id="btn-drop" class="btn btn-outline-dark" title="Add audio file(s)"
+            onClick={(event) => {
+              // To add audio files on smartphone device.
+              // By click on a button, rather than drop.
+              document.getElementById("input-drop").click()
+            }}
+          >
+            <i class="fas fa-plus"></i>
+          </button>
+          <input type="file" id="input-drop" multiple style="display: none;"
+            onChange={(event) => {
+              for (var i = 0; i < event.target.files.length; i++) {
+                ee.emit("newtrack", event.target.files[i]);
+              }
+            }}
+          ></input>
+        </div>
       </main>
     </>
   );
