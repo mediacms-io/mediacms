c63d3bf7a2d403f7af17b58cfb3530bf7711c9b1
Megidd <megiddgit@gmail.com>
2022-03-10 18:31:10 +0330

    Format

--- frontend/src/static/js/components/daw/index.js
+++ frontend/src/static/js/components/daw/index.js
@@ -278,48 +278,48 @@ export default function Daw() {
         </div>
         <div ref={container}></div>
         <div className="track-drop-outer">
-        <div className="track-drop" id="track-drop"
-          onDragEnter={(event) => {
-            event.preventDefault();
-            event.target.classList.add("drag-enter");
-          }}
-          onDragOver={(event)=>{
-            event.preventDefault();
-          }}
-          onDragLeave={(event)=>{
-            event.preventDefault();
-            event.target.classList.remove("drag-enter");
-          }}
-          onDrop={(event)=>{
-            event.preventDefault();
-            event.target.classList.remove("drag-enter");
-
-            // Fix:
-            // Uncaught TypeError: Cannot read properties of undefined (reading 'dataTransfer')
-            var dropEvent = event //event.originalEvent;
-
-            for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
-              ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
-            }
-          }}
-        >
-          <button type="button" id="btn-drop" className="btn btn-outline-dark" title="Add audio file(s)"
-            onClick={(event) => {
-              // To add audio files on smartphone device.
-              // By click on a button, rather than drop.
-              document.getElementById("input-drop").click()
+          <div className="track-drop" id="track-drop"
+            onDragEnter={(event) => {
+              event.preventDefault();
+              event.target.classList.add("drag-enter");
             }}
-          >
-            <i className="fas fa-plus"></i>
-          </button>
-          <input type="file" id="input-drop" multiple
-            onChange={(event) => {
-              for (var i = 0; i < event.target.files.length; i++) {
-                ee.emit("newtrack", event.target.files[i]);
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDragLeave={(event) => {
+              event.preventDefault();
+              event.target.classList.remove("drag-enter");
+            }}
+            onDrop={(event) => {
+              event.preventDefault();
+              event.target.classList.remove("drag-enter");
+
+              // Fix:
+              // Uncaught TypeError: Cannot read properties of undefined (reading 'dataTransfer')
+              var dropEvent = event //event.originalEvent;
+
+              for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
+                ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
               }
             }}
-          ></input>
-        </div>
+          >
+            <button type="button" id="btn-drop" className="btn btn-outline-dark" title="Add audio file(s)"
+              onClick={(event) => {
+                // To add audio files on smartphone device.
+                // By click on a button, rather than drop.
+                document.getElementById("input-drop").click()
+              }}
+            >
+              <i className="fas fa-plus"></i>
+            </button>
+            <input type="file" id="input-drop" multiple
+              onChange={(event) => {
+                for (var i = 0; i < event.target.files.length; i++) {
+                  ee.emit("newtrack", event.target.files[i]);
+                }
+              }}
+            ></input>
+          </div>
         </div>
       </main>
     </>
