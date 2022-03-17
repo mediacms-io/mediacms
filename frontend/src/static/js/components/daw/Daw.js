474d97e16206071deab03ae1d2345f7359f36ee1
Megidd <megiddgit@gmail.com>
2022-03-17 07:56:52 +0330

    New component for DAW track drop

--- frontend/src/static/js/components/daw/Daw.js
+++ frontend/src/static/js/components/daw/Daw.js
@@ -13,6 +13,7 @@ import '../daw/responsive.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import DawVideoPreview from './DawVideoPreview'
+import DawTrackDrop from "./DawTrackDrop";
 
 // See source code of this example:
 // https://naomiaro.github.io/waveform-playlist/web-audio-editor.html
@@ -288,50 +289,7 @@ export default function Daw({playerInstance}) {
           </div>
         </div>
         <div ref={container}></div>
-        <div className="track-drop-outer">
-          <div className="track-drop" id="track-drop"
-            onDragEnter={(event) => {
-              event.preventDefault();
-              event.target.classList.add("drag-enter");
-            }}
-            onDragOver={(event) => {
-              event.preventDefault();
-            }}
-            onDragLeave={(event) => {
-              event.preventDefault();
-              event.target.classList.remove("drag-enter");
-            }}
-            onDrop={(event) => {
-              event.preventDefault();
-              event.target.classList.remove("drag-enter");
-
-              // Fix:
-              // Uncaught TypeError: Cannot read properties of undefined (reading 'dataTransfer')
-              var dropEvent = event //event.originalEvent;
-
-              for (var i = 0; i < dropEvent.dataTransfer.files.length; i++) {
-                ee.emit("newtrack", dropEvent.dataTransfer.files[i]);
-              }
-            }}
-          >
-            <button type="button" id="btn-drop" className="btn btn-outline-dark" title="Add audio file(s)"
-              onClick={(event) => {
-                // To add audio files on smartphone device.
-                // By click on a button, rather than drop.
-                document.getElementById("input-drop").click()
-              }}
-            >
-              <i className="fas fa-plus"></i>
-            </button>
-            <input type="file" id="input-drop" multiple
-              onChange={(event) => {
-                for (var i = 0; i < event.target.files.length; i++) {
-                  ee.emit("newtrack", event.target.files[i]);
-                }
-              }}
-            ></input>
-          </div>
-        </div>
+        <DawTrackDrop ee={ee}></DawTrackDrop>
       </main>
     </>
   );
