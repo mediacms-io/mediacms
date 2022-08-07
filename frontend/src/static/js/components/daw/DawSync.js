b0bbaef0f4b8e717769ecb4d403e086b8970d821
Megidd <megiddgit@gmail.com>
2022-08-07 15:38:13 +0430

    Style `sync` just like `track drop`

--- frontend/src/static/js/components/daw/DawSync.js
+++ frontend/src/static/js/components/daw/DawSync.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function DawSync() {
     return (
         <div className="daw-sync-outer">
-            <div className="daw-sync">
+            <div className="daw-sync" id="daw-sync">
                 <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M8.35 40v-3h6.5l-.75-.6q-3.2-2.55-4.65-5.55-1.45-3-1.45-6.7 0-5.3 3.125-9.525Q14.25 10.4 19.35 8.8v3.1q-3.75 1.45-6.05 4.825T11 24.15q0 3.15 1.175 5.475 1.175 2.325 3.175 4.025l1.5 1.05v-6.2h3V40Zm20.35-.75V36.1q3.8-1.45 6.05-4.825T37 23.85q0-2.4-1.175-4.875T32.75 14.6l-1.45-1.3v6.2h-3V8h11.5v3h-6.55l.75.7q3 2.8 4.5 6t1.5 6.15q0 5.3-3.1 9.55-3.1 4.25-8.2 5.85Z"/></svg>
             </div>
         </div>
