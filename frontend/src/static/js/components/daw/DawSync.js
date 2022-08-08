6715c82203c53df6dcd7b0d26e72602ea174e073
Megidd <megiddgit@gmail.com>
2022-08-08 10:19:04 +0430

    `Save` button icon & color

--- frontend/src/static/js/components/daw/DawSync.js
+++ frontend/src/static/js/components/daw/DawSync.js
@@ -26,7 +26,7 @@ export default function DawSync({ ee }) {
           <button
             type="button"
             id="btn-drop"
-            className="btn btn-outline-dark"
+            className="btn btn-outline-primary"
             title="Save the current work as a voice file"
             onClick={(event) => {
               // Sync voices with database.
@@ -34,21 +34,7 @@ export default function DawSync({ ee }) {
               submitVoice();
             }}
           >
-            <svg
-              width="32"
-              height="32"
-              version="1.1"
-              viewBox="0 0 96 96"
-            >
-              <g fill="none" stroke="#422ca6" stroke-linecap="round" stroke-linejoin="round" stroke-width="4.5">
-                <path d="m33.286 14.25 7.1558 12.11 14.587 6.0549-7.431 11.009 0.55044 4.6788-17.889 5.5045" />
-                <path d="m19.25 31.589c6.6053 5.2292 12.66-3.5779 12.66-3.5779" />
-                <path d="m46.222 69.019c-17.064-3.5779-15.688-15.963-15.688-15.963" />
-                <path d="m55.579 53.607c9.6328 10.183 0.82567 15.963 0.82567 15.963" />
-                <path d="m61.359 47.827c15.688 19.816 0.82567 29.999 0.82567 29.999" />
-                <path d="m68.24 41.497c19.541 26.146 0.27522 41.559 0.27522 41.559" />
-              </g>
-            </svg>
+            <i className="fas fa-download"></i>
           </button>
         </div>
       </div>
