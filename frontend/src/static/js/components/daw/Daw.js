d91f3fdca4c5fe6b3638ff5584054d3c227324e1
Micrufun <micrufun@gmail.com>
2022-08-13 14:43:02 +0430

    Revert "Fix an import"
    This reverts commit 71630e73ef1ec7bfe4ad788a35700023f772a506.

--- frontend/src/static/js/components/daw/Daw.js
+++ frontend/src/static/js/components/daw/Daw.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Script from "next/script";
 import EventEmitter from "events";
 import { MediaPageStore } from '../../utils/stores/';
-import { MediaPageActions } from '../../utils/actions/';
 import { MemberContext } from '../../utils/contexts/';
 
 import 'waveform-playlist/styles/playlist.scss';
