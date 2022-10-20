6c4b3c2622e945eb1672a6b7fab2df4572bebe61
Kyle Maas <kylemaasdev@gmail.com>
2022-10-20 21:51:57 +0000

    Wrap logo in h1 tag for search engines

--- frontend/src/static/js/components/page-layout/PageHeader/Logo.jsx
+++ frontend/src/static/js/components/page-layout/PageHeader/Logo.jsx
@@ -4,9 +4,11 @@ export const Logo = ({ src, loading = 'lazy', title, alt, href = '#' }) => {
   return src ? (
     <div className="logo">
       <a href={href} title={title}>
-        <span>
-          <img src={src} alt={alt || title} title={title} loading={loading} />
-        </span>
+	<h1>
+          <span>
+            <img src={src} alt={alt || title} title={title} loading={loading} />
+          </span>
+	</h1>
       </a>
     </div>
   ) : null;
