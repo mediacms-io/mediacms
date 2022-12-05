9a39253757afd7f88f0fd06d45b1450101feb420
Micrufun <micrufun@gmail.com>
2022-12-05 17:28:00 +0330

    Develop VoiceAction DB model

--- actions/models.py
+++ actions/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from files.models import Media
 from users.models import User
+from files.models import Voice
 
 USER_MEDIA_ACTIONS = (
     ("like", "Like"),
@@ -11,6 +12,14 @@ USER_MEDIA_ACTIONS = (
     ("rate", "Rate"),
 )
 
+USER_VOICE_ACTIONS = (
+    ("like", "Like"),
+    ("dislike", "Dislike"),
+    ("watch", "Watch"),
+    ("report", "Report"),
+    # Rating is meaningless for voice. Maybe?
+    #("rate", "Rate"),
+)
 
 class MediaAction(models.Model):
     """Stores different user actions"""
@@ -51,5 +60,41 @@ class MediaAction(models.Model):
             models.Index(fields=["session_key", "action"]),
         ]
 
-# TODO
-#class VoiceAction(models.Model):
+class VoiceAction(models.Model):
+    """Stores different user actions"""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_index=True,
+        blank=True,
+        null=True,
+        related_name="useractions_voice",
+    )
+    session_key = models.CharField(
+        max_length=33,
+        db_index=True,
+        blank=True,
+        null=True,
+        help_text="for not logged in users",
+    )
+
+    action = models.CharField(max_length=20, choices=USER_VOICE_ACTIONS, default="watch")
+    # keeps extra info, eg on report action, why it is reported
+    extra_info = models.TextField(blank=True, null=True)
+
+    voice = models.ForeignKey(Voice, on_delete=models.CASCADE, related_name="voiceactions")
+    action_date = models.DateTimeField(auto_now_add=True)
+    remote_ip = models.CharField(max_length=40, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        super(VoiceAction, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.action
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user", "action", "-action_date"]),
+            models.Index(fields=["session_key", "action"]),
+        ]
