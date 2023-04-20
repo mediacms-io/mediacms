d11d775fea43e52e2e67bf0112c2e72715191dd7
Micrufun <micrufun@gmail.com>
2023-04-20 20:08:42 +0330

    Signal/slot for playlist tracks info

--- frontend/src/static/js/components/daw/DawDownload.js
+++ frontend/src/static/js/components/daw/DawDownload.js
@@ -3,12 +3,17 @@ import React from 'react'
 import './DawDownload.css';
 
 export default function DawDownload({ ee, playerInstance }) {
+    ee.on("inforesponse", (info)=> {
+        console.log('playlist info', info)
+    })
+
     return (
         <div className="daw-download-outer">
             <div className="daw-download" id="daw-download">
                 <button type="button" className="btn btn-outline-dark" title="Download video + displayed voices"
                     onClick={(event) => {
                         console.log('event: ', event)
+                        ee.emit("inforequest")
                     }}
                 >
                     <i className="fas fa-download"></i>
