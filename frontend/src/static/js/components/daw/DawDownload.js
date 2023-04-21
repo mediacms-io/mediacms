91f5f424827ae4a9e72537100b0575583af9b231
Micrufun <micrufun@gmail.com>
2023-04-21 12:48:31 +0330

    Remove signal/slot way to access displayed voices

--- frontend/src/static/js/components/daw/DawDownload.js
+++ frontend/src/static/js/components/daw/DawDownload.js
@@ -5,17 +5,12 @@ import { MediaPageStore } from '../../utils/stores/';
 import './DawDownload.css';
 
 export default function DawDownload({ ee, playerInstance }) {
-    ee.on("inforesponse", (info)=> {
-        console.log('playlist info', info)
-    })
-
     return (
         <div className="daw-download-outer">
             <div className="daw-download" id="daw-download">
                 <button type="button" className="btn btn-outline-dark" title="Download video + displayed voices"
                     onClick={(event) => {
                         console.log('event: ', event)
-                        ee.emit("inforequest")
                         const playlist = MediaPageStore.get('waveform-playlist');
                         if (playlist) {
                             console.log('playlist.getInfo()', playlist.getInfo())
