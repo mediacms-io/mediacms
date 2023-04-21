7b6a154f8a7a55e22aa5757a1da7e9eb74fa8415
Micrufun <micrufun@gmail.com>
2023-04-21 17:45:26 +0330

    New func

--- frontend/src/static/js/components/daw/DawDownload.js
+++ frontend/src/static/js/components/daw/DawDownload.js
@@ -5,16 +5,21 @@ import { MediaPageStore } from '../../utils/stores/';
 import './DawDownload.css';
 
 export default function DawDownload({ ee, playerInstance }) {
+
+    function encodeVideoWithVoices() {
+        const playlist = MediaPageStore.get('waveform-playlist');
+        if (playlist) {
+            console.log('playlist.getInfo()', playlist.getInfo())
+        }
+    }
+
     return (
         <div className="daw-download-outer">
             <div className="daw-download" id="daw-download">
                 <button type="button" className="btn btn-outline-dark" title="Download video + displayed voices"
                     onClick={(event) => {
                         console.log('event: ', event)
-                        const playlist = MediaPageStore.get('waveform-playlist');
-                        if (playlist) {
-                            console.log('playlist.getInfo()', playlist.getInfo())
-                        }
+                        encodeVideoWithVoices()
                     }}
                 >
                     <i className="fas fa-download"></i>
