dc868bf81642d28619cee7298975c7996261ae39
Micrufun <micrufun@gmail.com>
2023-04-21 20:48:38 +0330

    Rename func

--- frontend/src/static/js/components/daw/DawDownload.js
+++ frontend/src/static/js/components/daw/DawDownload.js
@@ -6,7 +6,7 @@ import './DawDownload.css';
 
 export default function DawDownload({ ee, playerInstance }) {
 
-    function encodeVideoWithVoices() {
+    function videoWithVoices() {
 
         // This value is set by a React effect after waveform-playlist is properly initialized.
         // So, we are sure that if it's not null or undefined, it would contain the info properly.
@@ -25,7 +25,7 @@ export default function DawDownload({ ee, playerInstance }) {
                 <button type="button" className="btn btn-outline-dark" title="Download video + displayed voices"
                     onClick={(event) => {
                         console.log('event: ', event)
-                        encodeVideoWithVoices()
+                        videoWithVoices()
                     }}
                 >
                     <i className="fas fa-download"></i>
