abe4c6369049157a477506dc25f33794fc909f23
Micrufun <micrufun@gmail.com>
2023-04-22 16:42:04 +0330

    See media on admin panel

--- actions/admin.py
+++ actions/admin.py
@@ -12,7 +12,7 @@ class MediaActionAdmin(admin.ModelAdmin):
 
 class VoiceActionAdmin(admin.ModelAdmin):
     search_fields = ["action"] # Some fields here cannot be searched by `icontains`
-    list_display = ["action", "extra_info", "user", "voice", "action_date", "remote_ip", "session_key"]
+    list_display = ["action", "extra_info", "user", "media", "action_date", "remote_ip", "session_key", "voice"]
     ordering = ("-action_date",)
 
 admin.site.register(MediaAction, MediaActionAdmin)
