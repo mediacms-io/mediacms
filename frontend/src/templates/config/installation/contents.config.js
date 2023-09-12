f15b2e6aca8a97e94981ed9972551135abb19643
Octavio Cano <ocano@blowleaf.com>
2023-09-12 16:40:12 +0900

    logo change

--- frontend/src/templates/config/installation/contents.config.js
+++ frontend/src/templates/config/installation/contents.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
     ],
     belowNavMenu: null,
-    footer: 'Powered by <a href="//demo.mediacms.io" title="mediacms.io" target="_blank">mediacms.io</a>',
+    footer: 'Powered by <a href="https://blowleaf.com" title="Blowleaf" target="_blank">blowleaf.com</a>',
   },
   uploader: {
     belowUploadArea: '',
