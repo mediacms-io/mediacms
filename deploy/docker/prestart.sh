dac09ddede759e7c967c4468919a0c2d7704fba9
therealmakervn <tech.@therealmaker.com>
2024-11-11 18:22:36 +0700

    config port15

--- deploy/docker/prestart.sh
+++ deploy/docker/prestart.sh
@@ -5,13 +5,6 @@ set -e
 export PORT=${PORT:-8000}
 echo "Starting uWSGI on port: $PORT"
 
-# Copy settings
-cp /home/mediacms.io/mediacms/deploy/docker/local_settings.py /home/mediacms.io/mediacms/cms/local_settings.py
-
-# Create necessary directories
-mkdir -p /home/mediacms.io/mediacms/{logs,media_files/hls}
-touch /home/mediacms.io/mediacms/logs/debug.log
-
 # Enable services
 if [ X"$ENABLE_UWSGI" = X"yes" ] ; then
     echo "Enabling uwsgi app server"
@@ -33,3 +26,8 @@ if [ X"$ENABLE_CELERY_LONG" = X"yes" ] ; then
     cp deploy/docker/supervisord/supervisord-celery_long.conf /etc/supervisor/conf.d/supervisord-celery_long.conf
     rm /var/run/mediacms/* -f
 fi
+
+# Create necessary directories
+mkdir -p /home/mediacms.io/mediacms/{logs,media_files/hls}
+touch /home/mediacms.io/mediacms/logs/debug.log
+chown -R www-data:www-data /home/mediacms.io/mediacms/
