b9fe03864d379aa09bd451e545716c5ed221be61
Markos Gogoulos <mgogoulos@gmail.com>
2025-02-10 09:59:58 +0200

    user validator

--- users/validators.py
+++ users/validators.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class ASCIIUsernameValidator(validators.RegexValidator):
-    regex = r"^[\w]+$"
+    regex = r"^[\w.@]+$"
     message = _("Enter a valid username. This value may contain only " "English letters and numbers")
     flags = re.ASCII
 
