b620b35f7d1adab4a3ddf895aa36ad8c5159ef8a
aleensd <aleen-sd@outlook.com>
2025-02-23 18:34:54 +0200

    update package scripts packages.json

--- frontend/packages/scripts/lib/.webpack/dev-server.config.ts
+++ frontend/packages/scripts/lib/.webpack/dev-server.config.ts
@@ -1,12 +1,16 @@
 import { Configuration } from 'webpack-dev-server';
 
-export function configFunc(contentBase: string): Configuration {
+export function configFunc(contentBase: string, host: string, port: number): Configuration {
   return {
     watchOptions: {
       poll: true,
     },
-    contentBase: contentBase,
+    static: {
+      directory: contentBase,
+    },
     compress: true,
     hot: true,
+    host: host,
+    port: port,
   };
 }
