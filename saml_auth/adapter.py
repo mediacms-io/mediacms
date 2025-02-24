31d356f92d8f34602e2e295e7d40cba2dc1688c5
Markos Gogoulos <mgogoulos@gmail.com>
2025-02-24 12:59:54 +0000

    a

--- saml_auth/adapter.py
+++ saml_auth/adapter.py
@@ -1,4 +1,8 @@
+import logging
+
+from django.dispatch import receiver
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.signals import social_account_updated, social_account_added
 
 class SAMLAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
@@ -12,9 +16,33 @@ class SAMLAccountAdapter(DefaultSocialAccountAdapter):
         user = sociallogin.user
         uid = sociallogin.account.uid
         user.username = sociallogin.account.uid
-        for item in ["name"]:
+        for item in ["name", "first_name", "last_name"]:
             if data.get(item):
                 setattr(user, item, data[item])
         sociallogin.data = data
+        # User is not retrieved through DB. Id is None.
+        
         return user
 
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        # Runs after new user is created
+        perform_user_actions(user)
+        return user
+
+@receiver(social_account_updated)
+def social_account_updated(sender, request, sociallogin, **kwargs):
+    # Runs after existing user is updated
+    user = sociallogin.user
+    perform_user_actions(user)
+
+
+def perform_user_actions(user):
+    try:
+        # set logo, if not set, and it exists
+        # get groups, create + add user (based in option)
+        # remove from groups (based in option)
+
+        except Exception as e:
+            logging.error(e)
+    return user
