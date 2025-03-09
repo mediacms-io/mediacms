a02e0a8a666f1a81b9309e19fa44d5ead2c49793
Markos Gogoulos <mgogoulos@gmail.com>
2025-03-09 20:48:09 +0200

    fix: flake8

--- files/views.py
+++ files/views.py
@@ -146,7 +146,7 @@ def edit_subtitle(request):
         if not filename.endswith(".vtt"):
             filename = f"{filename}.vtt"
 
-        response["Content-Disposition"] = f"attachment; filename={filename}"
+        response["Content-Disposition"] = f"attachment; filename={filename}"  # flake8: noqa
 
         return response
 
