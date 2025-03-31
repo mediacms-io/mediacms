02505aca0faffcfa700452ecb16853a4776d9c00
Markos Gogoulos <mgogoulos@gmail.com>
2025-03-31 10:27:24 +0000

    rbac categories

--- rbac/admin.py
+++ rbac/admin.py
@@ -25,7 +25,7 @@ class RoleFilter(admin.SimpleListFilter):
 
 class RBACGroupAdminForm(forms.ModelForm):
     categories = forms.ModelMultipleChoiceField(
-        queryset=Category.objects.filter(),
+        queryset=Category.objects.filter(is_rbac_category=True),
         required=False,
         widget=admin.widgets.FilteredSelectMultiple('Categories', False),
         help_text='Select categories this RBAC group has access to',
