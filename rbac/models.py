b9151560068f592e7aaa98854e41bc51df5743a0
Markos Gogoulos <mgogoulos@gmail.com>
2025-04-05 12:39:46 +0300

    remove unused code

--- rbac/models.py
+++ rbac/models.py
@@ -1,6 +1,5 @@
 from allauth.socialaccount.models import SocialApp
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
