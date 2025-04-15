de710915bc2e2c52ee5fcc640811e4883a855c8b
Yiannis Christodoulou <yiannis@web357.com>
2025-04-16 00:49:39 +0300

    chore: Remove unnecessary comments

--- frontend-tools/video-editor/client/src/App.tsx
+++ frontend-tools/video-editor/client/src/App.tsx
@@ -9,9 +9,7 @@ import Home from "@/pages/home";
 function Router() {
   return (
     <Switch>
-      {/* Add pages below */}
       <Route path="/" component={Home} />
-      {/* Fallback to 404 */}
       <Route component={NotFound} />
     </Switch>
   );
