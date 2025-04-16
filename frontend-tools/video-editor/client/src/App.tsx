4dac48521ab03c9134b2f994ec56afd83131bdd2
Yiannis Christodoulou <yiannis@web357.com>
2025-04-16 04:45:19 +0300

    chore: Remove the not-found component from react

--- frontend-tools/video-editor/client/src/App.tsx
+++ frontend-tools/video-editor/client/src/App.tsx
@@ -3,14 +3,12 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ToastComponent } from "@/components/Toast";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route component={NotFound} />
     </Switch>
   );
 }
