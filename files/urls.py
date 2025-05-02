618979561047e89e254f1bb9dc5e4e4f0a77b76e
Markos Gogoulos <mgogoulos@gmail.com>
2025-05-02 18:01:54 +0300

    url

--- files/urls.py
+++ files/urls.py
@@ -69,6 +69,10 @@ urlpatterns = [
         r"^api/v1/media/(?P<friendly_token>[\w]*)/chapters$",
         views.video_chapters,
     ),
+    re_path(
+        r"^api/v1/media/(?P<friendly_token>[\w]*)/trim_video$",
+        views.trim_video,
+    ),
     re_path(r"^api/v1/categories$", views.CategoryList.as_view()),
     re_path(r"^api/v1/tags$", views.TagList.as_view()),
     re_path(r"^api/v1/comments$", views.CommentList.as_view()),
