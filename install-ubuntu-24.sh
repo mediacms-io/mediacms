961271a0e268cc3229bd20b4572b19eeee515be9
Derek <derek.yates@live.com>
2025-05-12 19:24:16 +0000

    Update

--- install-ubuntu-24.sh
+++ install-ubuntu-24.sh
@@ -65,8 +65,8 @@ virtualenv . --python=python3
 source  ./bin/activate
 cd mediacms
 
-pip install --no-binary lxml lxml
-pip install --no-binary xmlsec xmlsec 
+pip install --no-binary lxml lxml==5.4.0
+pip install --no-binary xmlsec xmlsec==1.3.15 
 pip install -r requirements-ubuntu-24.txt
 
 SECRET_KEY=`python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'`
