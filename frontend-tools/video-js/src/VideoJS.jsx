697effbd639c95c96a9af9511f113cb5654196e5
Yiannis Christodoulou <yiannis@web357.com>
2025-09-18 11:15:26 +0300

    feat: Add a method to support player in the same page (e.g. for the embed page)

--- frontend-tools/video-js/src/VideoJS.jsx
+++ frontend-tools/video-js/src/VideoJS.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { VideoJSPlayer } from './components';
 
-function VideoJS() {
-    return <VideoJSPlayer />;
+function VideoJS({ videoId = 'default-video', ...props }) {
+    return <VideoJSPlayer videoId={videoId} {...props} />;
 }
 
 export default VideoJS;
