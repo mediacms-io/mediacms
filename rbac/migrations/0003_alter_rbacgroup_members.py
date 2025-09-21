725cc71960411944471b92baa4c8aaff7aa35b40
Markos Gogoulos <markos@sixfeetup.com>
2025-09-21 16:17:18 +0300

    fix migration

--- rbac/migrations/0003_alter_rbacgroup_members.py
+++ rbac/migrations/0003_alter_rbacgroup_members.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('rbac', '0002_alter_rbacgroup_uid'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
