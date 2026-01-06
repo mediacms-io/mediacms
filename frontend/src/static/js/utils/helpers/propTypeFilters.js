53a391da5e929e4c726a857cdcfd01cba8cd9753
Yiannis <1515939+styiannis@users.noreply.github.com>
2026-01-06 19:32:48 +0200

    fix code splitting issue

--- frontend/src/static/js/utils/helpers/propTypeFilters.js
+++ frontend/src/static/js/utils/helpers/propTypeFilters.js
@@ -1,38 +1,41 @@
 import { logErrorAndReturnError } from './errors';
-import { isPositiveInteger, isPositiveIntegerOrZero } from './math';
 
 export const PositiveIntegerOrZero = (function () {
-  return function (obj, key, comp) {
-    return void 0 === obj[key] || isPositiveIntegerOrZero(obj[key])
-      ? null
-      : logErrorAndReturnError([
-          'Invalid prop `' +
-            key +
-            '` of type `' +
-            typeof obj[key] +
-            '` supplied to `' +
-            (comp || 'N/A') +
-            '`, expected `positive integer or zero` (' +
-            obj[key] +
-            ').',
-        ]);
-  };
+    const isPositiveIntegerOrZero = (x) => x === Math.trunc(x) && x >= 0;
+
+    return function (obj, key, comp) {
+        return void 0 === obj[key] || isPositiveIntegerOrZero(obj[key])
+            ? null
+            : logErrorAndReturnError([
+                  'Invalid prop `' +
+                      key +
+                      '` of type `' +
+                      typeof obj[key] +
+                      '` supplied to `' +
+                      (comp || 'N/A') +
+                      '`, expected `positive integer or zero` (' +
+                      obj[key] +
+                      ').',
+              ]);
+    };
 })();
 
 export const PositiveInteger = (function () {
-  return function (obj, key, comp) {
-    return void 0 === obj[key] || isPositiveInteger(obj[key])
-      ? null
-      : logErrorAndReturnError([
-          'Invalid prop `' +
-            key +
-            '` of type `' +
-            typeof obj[key] +
-            '` supplied to `' +
-            (comp || 'N/A') +
-            '`, expected `positive integer` (' +
-            obj[key] +
-            ').',
-        ]);
-  };
+    const isPositiveInteger = (x) => x === Math.trunc(x) && x > 0;
+
+    return function (obj, key, comp) {
+        return void 0 === obj[key] || isPositiveInteger(obj[key])
+            ? null
+            : logErrorAndReturnError([
+                  'Invalid prop `' +
+                      key +
+                      '` of type `' +
+                      typeof obj[key] +
+                      '` supplied to `' +
+                      (comp || 'N/A') +
+                      '`, expected `positive integer` (' +
+                      obj[key] +
+                      ').',
+              ]);
+    };
 })();
