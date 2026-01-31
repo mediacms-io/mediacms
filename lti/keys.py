1a96ac07032aed586def3673fe98937f77fc7b2c
Markos Gogoulos <markos@sixfeetup.com>
2026-01-31 14:20:04 +0200

    t

--- lti/keys.py
+++ lti/keys.py
@@ -4,21 +4,6 @@ LTI Key Management for MediaCMS
 Manages RSA keys for signing Deep Linking responses (stored in database)
 """
 
-from jwcrypto import jwk
-
-
-def load_private_key():
-    """Load private key from database and convert to PEM format for PyJWT"""
-    from .models import LTIToolKeys
-
-    key_obj = LTIToolKeys.get_or_create_keys()
-
-    # Convert JWK dict to PEM string (PyJWT needs PEM format)
-    jwk_obj = jwk.JWK(**key_obj.private_key_jwk)
-    pem_bytes = jwk_obj.export_to_pem(private_key=True, password=None)
-
-    return pem_bytes.decode('utf-8')
-
 
 def load_public_key():
     """Load public key from database"""
