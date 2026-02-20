d28fc114f3dbc0827aeb76aaba1efb116366a089
Markos Gogoulos <markos@sixfeetup.com>
2026-02-20 12:17:13 +0200

    a

--- lti/urls.py
+++ lti/urls.py
@@ -9,14 +9,11 @@ from . import deep_linking, views
 app_name = 'lti'
 
 urlpatterns = [
-    # LTI 1.3 Launch Flow
     path('oidc/login/', views.OIDCLoginView.as_view(), name='oidc_login'),
     path('launch/', views.LaunchView.as_view(), name='launch'),
     path('jwks/', views.JWKSView.as_view(), name='jwks'),
     path('public-key/', views.PublicKeyPEMView.as_view(), name='public_key_pem'),
-    # Deep Linking
     path('select-media/', deep_linking.SelectMediaView.as_view(), name='select_media'),
-    # LTI-authenticated pages
     path('my-media/', views.MyMediaLTIView.as_view(), name='my_media'),
     path('embed/<str:friendly_token>/', views.EmbedMediaLTIView.as_view(), name='embed_media'),
 ]
