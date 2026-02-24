1df6e0c10d4da802b3de6e3f45768eee7f0075f6
Markos Gogoulos <markos@sixfeetup.com>
2026-02-24 10:57:56 +0200

    temp

--- files/views/categories.py
+++ files/views/categories.py
@@ -71,17 +71,17 @@ class CategoryListContributor(APIView):
         # Get global/public categories (non-RBAC)
         public_categories = Category.objects.filter(is_rbac_category=False).prefetch_related("user")
 
+        # Filter for LMS courses only if requested
+        lms_courses_only = request.GET.get('lms_courses_only', '').lower() in ['true', '1', 'yes']
+        if lms_courses_only:
+            categories = categories.filter(is_lms_course=True)
+
         # Get RBAC categories where user has contributor access
         if getattr(settings, 'USE_RBAC', False):
             rbac_categories = request.user.get_rbac_categories_as_contributor()
             categories = public_categories.union(rbac_categories)
         else:
             categories = public_categories
-
-        # Filter for LMS courses only if requested
-        lms_courses_only = request.GET.get('lms_courses_only', '').lower() in ['true', '1', 'yes']
-        if lms_courses_only:
-            categories = categories.filter(is_lms_course=True)
 
         categories = categories.order_by("title")
 
