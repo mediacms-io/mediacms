c356559bf512bf5d0e68d95b9a24c788458d290c
Markos Gogoulos <markos@sixfeetup.com>
2026-05-13 21:13:41 +0300

    wtv

--- frontend/src/static/js/utils/helpers/translate.js
+++ frontend/src/static/js/utils/helpers/translate.js
@@ -1,5 +1,5 @@
 // check templates/config/installation/translations.html for more
 
 export function translateString(str) {
-    return window.TRANSLATION?.[str] || str;
+    return window.TRANSLATION?.[str] ?? str;
 }
