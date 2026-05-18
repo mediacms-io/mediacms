c7a673bbbf46efc37621dc4a5109a85fc10e1317
Markos Gogoulos <mgogoulos@gmail.com>
2026-05-18 14:44:40 +0300

    fix: remove redundant check (#1528)

--- lti/apps.py
+++ lti/apps.py
@@ -1,16 +1,7 @@
 from django.apps import AppConfig
 
-from .keys import ensure_keys_exist
-
 
 class LtiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lti'
     verbose_name = 'LTI 1.3 Integration'
-
-    def ready(self):
-        """Initialize LTI app - ensure keys exist"""
-        try:
-            ensure_keys_exist()
-        except Exception:
-            pass
